17dd4087c89e66d9d2f22a0e5ccfcd7a7282effe
namlook <nicolas.clairon@elkorado.com>
2014-01-09 10:00:55 +0100

    add test for countdownView

--- test/test_views.js
+++ test/test_views.js
@@ -9,8 +9,11 @@ describe('TimerView', function() {
         var countdown = $('<h1 id="countdown"></h1>');
         var button = $('<button id="startButton">start</button>');
 
-        $('body').append(countdown);
-        $('body').append(button);
+        var testdiv = $('<div></div>');
+        testdiv.append(countdown);
+        testdiv.append(button);
+        testdiv.hide();
+        $('body').append(testdiv);
     });
 
     after(function () {
@@ -29,6 +32,25 @@ describe('TimerView', function() {
 
             startButtonView.$el.click();
             expect(countdownView.$el.text()).to.be.equal('25:00');
+        });
+        it('should decrement the clock every seconds', function() {
+            var timer = new Timer({'duration': 1500});
+            var countdownView = new CountdownView({
+                model: timer
+            });
+            var startButtonView = new StartButtonView({
+                model: timer
+            });
+
+            startButtonView.$el.click();
+            expect(countdownView.$el.text()).to.be.equal('25:00');
+
+            clock.tick(1000);
+            expect(countdownView.$el.text()).to.be.equal('24:59');
+            clock.tick(1000);
+            expect(countdownView.$el.text()).to.be.equal('24:58');
+            clock.tick(1400000);
+            expect(countdownView.$el.text()).to.be.equal('01:38');
         });
     });
 });
